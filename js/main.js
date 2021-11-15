b78d2c9d7ccd79aeb80e02e8ebf032484f9a7d82
arka777webdev <arka7772002@mail.ru>
2021-11-15 12:00:30 +0300

    fix bug

--- js/main.js
+++ js/main.js
@@ -3,5 +3,21 @@ $(function(){
     arrows:false,
     dots:true,
     slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 841,
+        settings: {
+          slidesToShow: 2,
+
+        }
+      },
+      {
+        breakpoint: 601,
+        settings: {
+          slidesToShow: 1,
+
+        }
+      },
+    ]
   });
 });
